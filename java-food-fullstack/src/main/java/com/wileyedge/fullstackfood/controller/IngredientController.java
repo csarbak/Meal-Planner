4fd79c9f263fca34fe0ec6f517565d469d6bd110
kevinl-gh <3dsifacebook@gmail.com>
2024-04-09 21:10:36 -0400

    Created the controllers for IngredientController.java and MealController.java

--- java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/controller/IngredientController.java
+++ java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/controller/IngredientController.java
@@ -1,10 +1,43 @@
 package com.wileyedge.fullstackfood.controller;
 
+import com.wileyedge.fullstackfood.model.Ingredient;
+import com.wileyedge.fullstackfood.service.IngredientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingredient")
 @CrossOrigin
 public class IngredientController {
+
+    @Autowired
+    IngredientServiceImpl ingredientService;
+
+    @GetMapping("/ingredients")
+    List<Ingredient> getAllIngredients() {
+        return ingredientService.getAllIngredients();
+    }
+
+    @GetMapping("/{id}")
+    public Ingredient getIngredientById(@PathVariable int id) {
+        return ingredientService.getIngredientById(id);
+    }
+
+    @PostMapping("/add")
+    public Ingredient addNewIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientService.addNewIngredient(ingredient);
+    }
+
+    @PutMapping("/{id}")
+    public Ingredient updateIngredientData(@PathVariable int id, @RequestBody Ingredient ingredient) {
+        return ingredientService.updateIngredientData(id, ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIngredientById(@PathVariable int id) {
+        ingredientService.deleteIngredientById(id);
+    }
 
 }
--- java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/controller/MealController.java
+++ java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/controller/MealController.java
@@ -1,11 +1,43 @@
 package com.wileyedge.fullstackfood.controller;
 
+import com.wileyedge.fullstackfood.model.Meal;
+import com.wileyedge.fullstackfood.service.MealServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/meal")
 @CrossOrigin
 public class MealController {
+    @Autowired
+    MealServiceImpl mealService;
+
+    @GetMapping("/meals")
+    List<Meal> getAllMeals() {
+        return mealService.getAllMeals();
+    }
+
+    @GetMapping("/{id}")
+    public Meal getMealById(@PathVariable int id) {
+        return mealService.getMealById(id);
+    }
+
+    @PostMapping("/add")
+    public Meal addNewMeal(@RequestBody Meal meal) {
+        return mealService.addNewMeal(meal);
+    }
+
+    @PutMapping("/{id}")
+    public Meal updateMealData(@PathVariable int id, @RequestBody Meal meal) {
+        return mealService.updateMealData(id, meal);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMealById(@PathVariable int id) {
+        mealService.deleteMealById(id);
+    }
 
 
 }
