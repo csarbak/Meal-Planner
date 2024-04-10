7a93f213bb0761658fb11041d8595594184ec98d
kevinl-gh <3dsifacebook@gmail.com>
2024-04-10 13:47:04 -0400

    Debugged Spring annotations

--- java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/dao/UserDaoImpl.java
+++ java-food-fullstack/src/main/java/com/wileyedge/fullstackfood/dao/UserDaoImpl.java
@@ -5,9 +5,11 @@ import com.wileyedge.fullstackfood.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class UserDaoImpl implements UserDao {
     @Autowired
     private final JdbcTemplate jdbcTemplate;
